a0e51bff7f7b2aef9bff358ed1f62ac8556d8042
Nelson Daza <nelson.daza@gmail.com>
2024-11-08 20:02:38 -0500

    use observer for content changes

--- packages/linear/src/hooks/useDocumentCodes.ts
+++ packages/linear/src/hooks/useDocumentCodes.ts
@@ -1,4 +1,4 @@
-import { useMemo } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 
 export const useTitleCodes = () => {
   const title = document.querySelector('#partial-discussion-header h1[class*="title"]')?.textContent || ''
@@ -6,8 +6,27 @@ export const useTitleCodes = () => {
 }
 
 export const useCommentCodes = () => {
-  const comment = document.querySelector('task-lists > div[class*="comment-body"]')?.textContent || ''
-  return comment.match(/([A-Z]{2,}-\d+)/gm) || []
+  const [codes, setCodes] = useState<string[]>([])
+
+  useEffect(() => {
+    const contentNode = document.querySelector('.TimelineItem')
+
+    // Create an observer instance linked to the callback function
+    const observer = new MutationObserver(() => {
+      const content = contentNode?.textContent || ''
+      setCodes(content.match(/([A-Z]{2,}-\d+)/gm) || [])
+    })
+
+    // Start observing the target node for configured mutations
+    observer.observe(contentNode, { childList: true, subtree: true })
+
+    // Later, you can stop observing
+    return () => {
+      observer.disconnect()
+    }
+  }, [])
+
+  return codes
 }
 
 export const useDocumentCodes = () => {
@@ -24,6 +43,6 @@ export const useDocumentCodes = () => {
           return a.length - b.length
         })
         .filter((code, index, array) => array.lastIndexOf(code) === index),
-    [],
+    [titleCodes.toString(), commentCodes.toString()],
   )
 }
